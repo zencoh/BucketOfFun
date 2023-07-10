05ae90bd5730dc1918b4cb709ce8d51dbb74efab
Cole Hartkopp <chsoccer4@outlook.com>
2023-07-09 20:31:04 -0500

    React app boiler plate along with beginnings of app and index for bucket list

---
+++ bucket-of-fun/src/App.js
@@ -0,0 +1,11 @@
+// import app css and components
+
+function App() {
+    return (
+        <div className="bucket-app">
+            <BucketList />
+        </div>
+    );
+}
+
+export default App;
